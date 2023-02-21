9a917c0408801463003d584fdfda965a5b61c4de
Rea <andrea.solisg@gmail.com>
2023-02-21 23:02:34 +0100

    edit users settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users do
-  get '/users/sign_out' => 'devise/sessions#destroy'
-   end
+  devise_for :users 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users
   resources :events
   resources :attendances
   root "events#index"
