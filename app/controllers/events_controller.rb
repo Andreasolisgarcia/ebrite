4a8811a348f1bf52f8735dc52c54358e611e6160
Rea <andrea.solisg@gmail.com>
2023-02-22 01:57:12 +0100

    mail welcome, crud user,

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,13 +16,22 @@ class EventsController < ApplicationController
     def create
       @event = Event.new(event_params)
       if @event.save
-        flash[:success] = " Your Gossip is here now !"
+        flash[:success] = " Your Event has been created"
         redirect_to  events_path   
      end
+    end
+    
+    def destroy
+      @event = Event.find(params[:id])
+      @event.destroy
+      flash[:success] = "Event deleted succesfully"
+      redirect_to events_path
     end
   
     private
     def event_params
-      params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location)
+      params.require(:event).permit(:title, :location, :description, :start_date, :duration, :price)
     end
 end
+
+
